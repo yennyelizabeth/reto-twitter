a4b1d85c037d436e158ae943237fe11ce422ec13
yennyelizabeth <yenny.elizabeth@hotmail.com>
2017-11-15 17:46:14 -0500

    añadiendo estilo guia

--- app.js
+++ app.js
@@ -1,64 +1,58 @@
 var parameterLimitCharacters = 140;
-var regressive=0;
+var regressive = 0;
 
-window.addEventListener('load', function(event){
+window.addEventListener('load', function(event) {
   var button = document.getElementById('button');
 
   /* Evento en el Botón */
-  button.addEventListener('click', function(event){
+  button.addEventListener('click', function(event) {
     var textarea = document.getElementById('textarea');
-    var textAreaText = textarea.value;
+    var textAreaText = textarea.value ;
     alert(textAreaText);
 
     /* ubico área para crear elementos */
-    var sectionDivs = document.querySelector("section"); /* selecciono section*/
+    var sectionDivs = document.querySelector('section'); /* selecciono section*/
 
     /* creación de elemento p y nodo texto text */
-    var p = document.createElement("p");
-    var text =document.createTextNode(textAreaText);
+    var pVar = document.createElement('p') ;
+    var text = document.createTextNode(textAreaText);
 
     /* asignar hijos a elementos seleccionados */
-    p.appendChild(text);
-    sectionDivs.appendChild(p);
+    pVar.appendChild(text);
+    sectionDivs.appendChild(pVar);
 
+    textarea.value = '' ;
+  });
 
+  /* Evento Presión de Tecla */
+  textarea.addEventListener('keypress', function(event) {
+    var textAreaText = textarea.value;
+    var label = document.getElementById('label');
+    label.classList.remove('red', 'green', 'blue');
+    label.classList.add('black');
 
+    /* asigna la cantidad de caracteres  */
+    if (textAreaText.length > 0) {
+      regressive = parameterLimitCharacters - textAreaText.length ;
+      label.textContent = regressive ;
+    }
 
-  }) ;
+    if (regressive > 130 && regressive <= 140) {
+      label.classList.add('black');
+    }
 
-    /* Evento Presión de Tecla */
-    textarea.addEventListener('keypress', function(event){
-      var textAreaText = textarea.value;
-      var label = document.getElementById('label');
-         label.classList.remove("red" ,"green" ,"blue");
-         label.classList.add("black");
+    if (regressive > 120 && regressive <= 130) {
+      label.classList.add('red');
+    }
 
-      /* asigna la cantidad de caracteres  */
-      if (textAreaText.length >0) {
-        regressive = parameterLimitCharacters-textAreaText.length ;
-        label.textContent = regressive ;
-      }
+    if (regressive <= 120 && regressive >= 0) {
+      label.classList.remove('red');
+      label.classList.add('blue');
+    }
 
-      if (regressive > 130  && regressive <= 140  ) {
-         label.classList.add("black");
-      }
-
-      if (regressive > 120  && regressive <= 130  ) {
-         label.classList.add("red");
-      }
-
-      if (regressive <= 120  && regressive >= 0 ){
-         label.classList.remove("red");
-         label.classList.add("blue");
-      }
-
-      if (regressive < 0 ){
-         label.classList.remove("blue");
-         label.classList.add("green");
-      }
-
-
-
-    });
-
+    if (regressive < 0) {
+      label.classList.remove('blue');
+      label.classList.add('green');
+    }
+  });
 });
